20c3b5c8beae71bbd5cae792b9ba647b95d2cd44
Gregory Sanders <grysar@zort.net>
2019-01-21 17:22:37 -0500

    ProductOrService fix

--- lookups.r
+++ lookups.r
@@ -1157,7 +1157,7 @@ apply_lookups<- function(VAR.path,VAR.df){
     colnames(VAR.df)[colnames(VAR.df)=="ProductOrServiceArea"]<-"ProductServiceOrRnDarea"
     VAR.df<-read_and_join(VAR.df,
                                    "LOOKUP_Buckets.csv",by="ProductServiceOrRnDarea")
-    colnames(VAR.df)[colnames(VAR.df)=="ProductServiceOrRnDarea"]<-"ProductOrServiceArea"
+    colnames(VAR.df)[colnames(VAR.df)=="ProductServiceOrRnDarea"]<-"ProductServiceOrRnDarea"
     
     NA.check.df<-subset(VAR.df, is.na(ServicesCategory.sum), select=c("ProductOrServiceArea"))
     if(nrow(NA.check.df)>0){
